9fe8a7fc55759ef84ed64dd64d7bab35806e7014
szlahorek.andras <szlahorek.andras@192.168.0.60>
2022-11-25 00:58:25 +0100

    8_het

--- security/src/main/java/hu/webuni/security/config/SecurityConfig.java
+++ security/src/main/java/hu/webuni/security/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package hu.webuni.security.config;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 //@EnableWebSecurity
 public class SecurityConfig {
 
