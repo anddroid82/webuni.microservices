5f76a71d1b232a275c0415b62a9547ec610d7452
szlahorek.andras <szlahorek.andras@192.168.0.60>
2022-11-25 01:11:30 +0100

    8_het

--- security/src/main/java/hu/webuni/security/web/Controller.java
+++ security/src/main/java/hu/webuni/security/web/Controller.java
@@ -1,12 +1,14 @@
 package hu.webuni.security.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/security")
+@RequestMapping("/api")
 public class Controller {
 
+	@GetMapping("/test")
 	public void test() {
 		
 	}
