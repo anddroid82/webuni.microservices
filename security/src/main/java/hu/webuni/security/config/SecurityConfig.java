9cedcfc6836b2bbb54aaf6523b9dd01016e1e847
szlahorek.andras <szlahorek.andras@10.2.14.212>
2022-11-25 11:45:12 +0100

    8_het

--- security/src/main/java/hu/webuni/security/config/SecurityConfig.java
+++ security/src/main/java/hu/webuni/security/config/SecurityConfig.java
@@ -1,8 +1,55 @@
 package hu.webuni.security.config;
 
-//@EnableWebSecurity
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
+@Configuration
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//@Autowired
+	//JwtAuthFilter jwtAuthFilter;
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("user").authorities("view").password(passwordEncoder().encode("password"))
+			.and()
+			.withUser("admin").authorities("view","search").password(passwordEncoder().encode("password"));
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.antMatchers("/api").denyAll();
+		//http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 	
 }
